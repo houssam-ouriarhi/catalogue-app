9289aff08d124f6ff19531588012250b1d42f3af
Houssam OURIARHI <ouriarhi.houssam@gmail.com>
2023-05-18 13:00:53 +0100

    Creating Navigation Footer and product ist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/shared/navigation/navigation.component';
+import { ProductsListComponent } from './components/products/products-list/products-list.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProductsListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
